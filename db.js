ee9f49006ce85b4e9efda289af184c6578826541
Anne <northofnormal@gmail.com>
2014-01-10 13:57:48 -0500

    Forgot to commit initiallyHTML JS and CSS files created

---
+++ db.js
@@ -0,0 +1,69 @@
+var todoDB = (function(){
+  var tDB = {};
+  var datastore = null; 
+    tDB.open = function(callback) {
+      var version = 1;
+      var request = indexedDB.open('todos', version);
+      request.onupgradeneeded = function(e) {
+        var db = e.target.result;
+        e.target.transaction.onerror = tDB.onerror;
+        if(db.objectStoreName.contains('todo')) {
+          db.deleteObjectStore('todo');
+        }
+        var store = db.createObjectStore('todo', {
+          keyPath: 'timestamp'
+        });
+      };
+      request.onsuccess = function(e) {
+        datastore = e.target.result;
+        callback();
+      };
+      request.onerror = tDB.onerror;
+    };
+
+    tDB.fetchTodos = function(callback) {
+      var db = datastore;
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var objStore = transaction.objectStore('todo');
+
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = objStore.openCursor(keyRange);
+
+      var todo = [];
+
+      transactiononcomplete = function(e) {
+        callback(todos);
+      };
+
+      cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+
+        if (!!result == false) {
+          return;
+        }
+
+        todos.push(result.value);
+
+        result.continue();
+      };
+
+      cursorRequest.onerror = tDB.onerror;
+    };
+
+    tDB.createTodo = function(text, callback) {
+      var db = datastore;
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var objStore = transaction.objectStore('todo');
+      var timestamp = new Date().getTime();
+
+      var todo = {
+        'text': text, 
+        'timestamp': timestamp
+      };
+
+      
+    }
+
+  return tDB; 
+}());
+
